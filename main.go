3d44b01caa7ac3421c97353c8af88f9568a59464
hexygopher <hexygopher@gmail.com>
2015-11-14 16:44:48 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,42 +1,47 @@
 package main
+
 import (
-    "net/http"
-    "io/ioutil"
-    "strings"
-    "log"
-    "github.com/stumpyfr/udger"
+	"github.com/stumpyfr/udger"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
+
 type MyHandler struct {
-    u *udger.Udger
+	u *udger.Udger
 }
+
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    path := r.URL.Path[1:]
-    if path == "" { path = "main.html" }
-    agent := r.UserAgent()
-    log.Println(strings.Split(r.RemoteAddr,":")[0] + " reqested", "/"+path)
-    ua, err := this.u.Lookup(agent)
-    if err != nil {
-        log.Println("error:", err)
-    }
-    log.Printf("%+v\n", ua)
-    data, err := ioutil.ReadFile(string(path))
-    if err == nil {
-        w.Write(data)
-    } else {
-        data, _ = ioutil.ReadFile("404.html")
-        w.WriteHeader(404)
-        w.Write(data)
-    }
+	path := r.URL.Path[1:]
+	if path == "" {
+		path = "main.html"
+	}
+	agent := r.UserAgent()
+	log.Println(strings.Split(r.RemoteAddr, ":")[0]+" reqested", "/"+path)
+	ua, err := this.u.Lookup(agent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%+v\n", ua)
+	data, err := ioutil.ReadFile(string(path))
+	if err == nil {
+		w.Write(data)
+	} else {
+		data, _ = ioutil.ReadFile("404.html")
+		w.WriteHeader(404)
+		w.Write(data)
+	}
 }
 func main() {
-    handler := new(MyHandler)
-    var err error
-    handler.u, err = udger.New("udgerdb.dat")
-    if err == nil {
-        log.Println("UdgerDB loaded successfully")   
-    } else {
-        log.Println("error:", err)
-    }
-    http.Handle("/", handler)
-    http.ListenAndServe(":8080", nil)
+	handler := new(MyHandler)
+	var err error
+	handler.u, err = udger.New("udgerdb.dat")
+	if err == nil {
+		log.Println("UdgerDB loaded successfully")
+	} else {
+		log.Fatal(err)
+	}
+	http.Handle("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
