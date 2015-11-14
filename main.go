5c2ff985eaaf3700a74f460de0598e640a77463e
hexygopher <hexygoper@gmail.com>
2015-11-14 22:06:28 +0200

    edited

--- main.go
+++ main.go
@@ -17,21 +17,25 @@ type MyHandler struct {
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	agent := r.UserAgent()
 	ua, err := this.u.Lookup(agent)
+	
 	if err != nil {
 		log.Fatal(err)
 	}
+	
 	path := r.URL.Path[1:]
 	device := ua.Device.Name
 	os := ua.OS.Family
 	browser := ua.Browser.Name
-	if (r.Method == "POST"){
+
+	if r.Method == "POST" {
 		if err = r.ParseForm(); err != nil{
 			log.Fatal(err)
 		}
+
 		form := r.PostForm
 		log.Println(strings.Split(r.RemoteAddr, ":")[0] + " reqested", "/" + path, "\ndevice:", device, "\nOS:", os, "\nBrowser:", browser, "\nUser-Agent: " + agent, fmt.Sprintf("\nForm=%v",form))
 
-	} else if r.Method == "GET"{
+	} else if r.Method == "GET" {
 		if path == "" {
 			if device != "Smartphone" {
 				path = "index.html"
@@ -49,13 +53,16 @@ func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(404)
 			w.Write(data)
 		}
+
 		log.Println(strings.Split(r.RemoteAddr, ":")[0] + " reqested", "/"+path, "\ndevice:", device, "\nOS:", os, "\nBrowser:", browser, "\nUser-Agent: " + agent)
 	}
 }
 func main() {
 	var err error
 	port := flag.Int("p", 8080, "port on which the server runs")
+
 	flag.Parse()
+	
 	handler := new(MyHandler)
 	handler.u, err = udger.New("udgerdb.dat")
 	
@@ -66,6 +73,7 @@ func main() {
 	}
 	
 	http.Handle("/", handler)
+
 	if err = http.ListenAndServe(fmt.Sprintf(":%d",*port), nil); err != nil{
 		log.Fatal(err)
 	}
